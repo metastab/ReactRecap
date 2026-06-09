61ac98dba7c38d9feee3af48f501d64d1a559c6d
Bablu Kumar <miller03ray@gmail.com>
2026-06-09 21:46:16 +0530

    PROPSreact
    mini fun site to practice props in react.

---
+++ whiskers_project/src/contacts.jsx
@@ -0,0 +1,28 @@
+export default function Contacts(props) {
+    console.log(props)
+
+    let src = props.img;
+    let name = props.name;
+    let phone = props.phone;
+    let email = props.email;
+
+    return (
+        <article className = 'contact-card'>
+
+            <img className='profile' src={src} alt={name} />
+
+            <h2>{name}</h2>
+
+            <div className="info-group">
+                <img src="images/phone-icon.png" alt="phone" width="15" height="15" />
+                <p>{phone}</p>
+            </div>
+
+            <div className="info-group">
+                <img src="images/mail-icon.png" alt="mail" width="15" height="15"/>
+                <p>{email}</p>
+            </div>
+
+        </article>
+    )
+}
---
+++ whiskers_project/src/main.jsx
@@ -0,0 +1,49 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+
+import Contacts from './contacts'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+
+    <div className='contacts-container'>
+      <Contacts img="./images/whiskerson.jpg"
+                name="Mr. Whiskerson"
+                phone="(212) 555-1234"
+                email="mr.whiskaz@catnap.meow"
+      />
+      <Contacts img="./images/luna-paws.jpg"
+                name="Luna Paws"
+                phone="(415) 555-8765"
+                email="luna.paws@mooncats.meow"
+      />
+      <Contacts img="./images/sir-pounce.jpg"
+                name="Sir Pounce"
+                phone="(718) 555-4321"
+                email="pounce.alot@zoomies.meow"
+      />
+      <Contacts img="./images/mittens.jpg"
+                name="Mittens McFluff"
+                phone="(646) 555-9087"
+                email="mittens@fluffmail.meow"
+      />
+      <Contacts img="./images/captain-tuna.jpg"
+                name="Captain Tuna"
+                phone="(310) 555-7788"
+                email="captain.tuna@seacats.meow"
+      />
+      <Contacts img="./images/shadow.jpg"
+                name="Shadow Beans"
+                phone="(503) 555-2468"
+                email="shadow.beans@midnight.meow"
+      />
+      <Contacts img="./images/chonkster.jpg"
+                name="Professor Chonk"
+                phone="(202) 555-1357"
+                email="prof.chonk@catademy.meow"
+      />
+    </div>
+    
+  </StrictMode>,
+)
