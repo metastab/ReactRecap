0fb10e65ea56f45b6390021d7094bdebc09c1514
Bablu Kumar <miller03ray@gmail.com>
2026-06-17 13:29:56 +0530

    conditional rendering

--- joker/src/body.jsx
+++ joker/src/body.jsx
@@ -3,9 +3,9 @@ import React from "react"
 
 export default function Body() {
 
-    const [keywords, setKeywords] = React.useState([]);
+    const [keywords, setKeywords] = React.useState(["banana", "sugar"]);
 
-    const keyword_list = keywords.map((keyword)=>{
+    const keyword_list = keywords.map((keyword) => {
         return (
             <li id="typed-keywords" key={keyword}>
                 {keyword}
@@ -17,13 +17,13 @@ export default function Body() {
         const newKeyword = formData.get("keywords");
 
         //empty check
-        if (newKeyword === ""){
+        if (newKeyword === "") {
             return
         }
 
         //add newKeyword to the array
-        setKeywords((prevKeywords)=>{
-            return(
+        setKeywords((prevKeywords) => {
+            return (
                 [...prevKeywords, newKeyword]
             )
         })
@@ -32,28 +32,34 @@ export default function Body() {
     return (
         <main>
             <form action={handleSubmit} className="keyword-form">
-                <input 
+                <input
                     type="text"
                     placeholder=" eg. rain"
                     aria-label="keywords"
-                    name="keywords" 
+                    name="keywords"
                 />
                 <button>+ Type keywords</button>
             </form>
 
-        {/* will be using conditional rendering here */}
-        {!keywords.length == 0 &&
-        <section>
-            <h2 id="form-header">Current keywords</h2>
-            <ul id="keyword-list" > {keyword_list} </ul>
-            <div className="get-keyword-container">
-                <div>
-                    <h3>Ready for a joke?</h3>
-                    <p>Generate a joke from your list of keywords.</p>
-                </div>
-                <button>🎭 Get a Joke.</button>
-            </div>
-        </section>}
+            {/* will be using conditional rendering here */}
+
+            {keywords.length < 3 &&
+                <h5 id="min-items-warning">please provide atleast 4 ingredients!</h5>}
+
+            <section>
+                <h2 id="form-header">Current keywords</h2>
+                <ul id="keyword-list" > {keyword_list} </ul>
+
+                {keywords.length >= 3 &&
+                    <div className="get-keyword-container">
+                        <div>
+                            <h3>Ready for a joke?</h3>
+                            <p>Generate a joke from your list of keywords.</p>
+                        </div>
+                        <button>🎭 Get a Joke.</button>
+                    </div>}
+
+            </section>
 
         </main>
     )
